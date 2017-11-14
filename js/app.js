4a31f910dff20f9aa7c30eebdc047a7e50dbe391
CredieNogueiraDeLima <uh6456jv@uh6456jvm7.workstations.winona.edu>
2017-11-14 15:15:33 -0600

    intitial status form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
+//WE WILL STORE DATA IN JAVASCRIPT UNTIL FINAL SUBMIT BUTTON SENDS USING AJAX TO PHP
 $( document ).ready(function() {
     console.log( "ready!" );
+
+    let status;
+    //events
+    $("input:radio[name=status]").change(function(){
+        // console.log("changed");
+
+        let statusInput = $("input:radio[name=status]:checked").val();
+        // console.log(statusInput);
+
+        if(statusInput == "student") {
+            console.log("student!");
+            $("#studentBox").slideDown("fast");
+        } else {
+            $("#studentBox").slideUp("fast");
+        }
+    });
+
+    // $("#statusButtonSubmit").on("click",function(){
+    //     setStatus(moveOn);
+    // });
+
+    // 
+    
+
+    function setStatus (moveOn) {
+       
+        let statusInput = $("input:radio[name=status]:checked").val();
+        status = statusInput;
+        console.log(status); 
+
+        $("#statusForm").toggleSlideDown();
+    }
+
+    function moveOn() {
+     
+        console.log("do it");
+    }
+
 });
--- userTypeForm.php
+++ userTypeForm.php
@@ -3,14 +3,66 @@
 <?php include "nav.php"; ?>
 
 <body>
-    <h1 id="formTitle">What Are You?</h1>
-    <div class="form">
-		<p class="radioContainer">
-			<label><input type = "radio" name = "status" value = "student"> Student </label> 
-			<label><input type = "radio" name = "status" value = "faculty"> Faculty </label>
-			<label><input type = "radio" name = "status" value = "staff"> Staff </label>
-		</p>
-		<button class="Button">Next</button>
+	<h1 id="formTitle">What Are You?</h1>
+	<hr id="titleHR">
+
+    <div id="statusForm" class=" radioContainer">
+
+	<div class="inputs">
+			
+		<div class="pretty p-icon p-curve p-jelly">
+        	<input type="radio" name="status" value="student" >
+        	<div class="state p-success">
+				<i class="icon fa fa-check"></i>
+            	<label> Student</label>
+        	</div>
+		</div>
+
+	
+
+		<div class="pretty p-icon p-curve p-jelly">
+        	<input type="radio" name="status" value="faculty" checked="checked">
+        	<div class="state p-success">
+				<i class="icon fa fa-check"></i>
+            	<label> Faculty</label>
+        	</div>
+		</div>
+
+
+
+		<div class="pretty p-icon p-curve p-jelly">
+        	<input type="radio" name="status" value="staff">
+        	<div class="state p-success">
+				<i class="icon fa fa-check"></i>
+            	<label> Staff</label>
+        	</div>
+		</div>
+
+		<div id="studentBox">
+			<h5 id="studentGroupTitle">As a student this is a</h5>
+
+			<div class="pretty p-icon p-curve p-jelly">
+        		<input type="radio" name="group" value="alone">
+        		<div class="state p-success">
+					<i class="icon fa fa-check"></i>
+            		<label>Solo Project</label>
+        		</div>
+			</div>
+			<br>
+			<div class="pretty p-icon p-curve p-jelly">
+        		<input type="radio" name="group" value="group">
+        		<div class="state p-success">
+					<i class="icon fa fa-check"></i>
+            		<label>Group Project</label>
+        		</div>
+			</div>
+
+		</div>
+
+		</div>
+
+		<!-- </p> -->
+		<button id="statusButtonSubmit" class="Button">Next</button>
 	</div>
 
 </body>
