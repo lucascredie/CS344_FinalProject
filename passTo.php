30692e68a8b39d6c9519945c065fa2a25fefab2f
mhalleck <mhalleck98@gmail.com>
2017-11-17 13:10:16 -0600

    Deleted erroneous files

---
+++ passTo.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html5>
+<?php
+	$servername = "localhost";
+	$username = "admin";
+	$password = "1234";
+	$conn = new mysqli($servername, $username, $password);
+	
+	
+?>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title> Submission Confirmation </title>
+		<link href="index.css" rel="stylesheet"/>
+		
+		
+	</head>
+	
+	<body>
+		<header>
+		</header>
+		
+		
+		<main>
+		
+		</main>
+		
+		<footer>
+		</footer>
+		
+	</body>
+
+
+
+
+</html>
